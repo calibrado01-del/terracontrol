1a1315b793b82f35f172f517508da6799f464512
calibrado01-del <calibrado01@gmail.com>
2026-05-22 16:03:55 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 // Preencha com os seus dados do Supabase (passo 2 do guia)
 // ============================================================
 
-const SUPABASE_URL = 'Wjssgqcybqapttjstbpi';
+const SUPABASE_URL = 'wjssgqcybqapttjstbpi';
 const SUPABASE_KEY = 'us-west-1';
